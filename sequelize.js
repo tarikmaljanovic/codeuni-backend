6d16504614b52d8f59abbaa854a44c0303da8b71
tarikmaljanovic <tarikmaljanovic123@gmail.com>
2025-06-15 14:05:04 +0200

    port

--- sequelize.js
+++ sequelize.js
@@ -5,6 +5,7 @@ configDotenv()
 
 const sequelize = new Sequelize(process.env.DB_SCHEMA, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'mysql'
 })
 
